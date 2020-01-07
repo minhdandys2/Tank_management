163569bd42ea08e12140b6c2cb196069e7302f5b
user_name <email_id>
2020-01-07 09:13:03 +0700

    task management

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Customer;
+use Illuminate\Http\Request;
+
+class CustomerController extends Controller
+{
+    public function __construct()
+    {
+//        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        $customers = Customer::all();
+        return response()->json($customers);
+    }
+
+    public function create(){
+        return view('create');
+    }
+    public function store(Request $request){
+        $customer = new Customer();
+        $customer->name = $request->name;
+        $customer->age = $request->age;
+        $customer->email = $request->email;
+        $customer->address = $request->address;
+
+        $customer->save();
+        return redirect()->route('customer.listCustomer');
+    }
+    public function delete($id){
+        $customer = Customer::findOrFail($id);
+        $customer->delete();
+        return redirect()->route('customer.listCustomer');
+    }
+    public function edit($id){
+        $customer = Customer::findOrFail($id);
+        return view('edit',compact('customer'));
+    }
+    public function update(Request $request,$id){
+          $customer = Customer::findOrFail($id);
+        $customer->name = $request->name;
+        $customer->email = $request->email;
+        $customer->address = $request->address;
+        $customer->save();
+//        $customer->update($request->all());
+        return redirect()->route('customer.listCustomer');
+    }
+}
